1751e1496badcc618eeaa32b12f6bff784f033c0
Lili-Jeanne <lili0jeanne0flourez@gmail.com>
2025-01-31 17:40:04 +0100

    Update delete.php

--- views/backend/keywords/delete.php
+++ views/backend/keywords/delete.php
@@ -11,13 +11,13 @@ if(isset($_GET['numMotCle'])){
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Suppression de la thematique</h1>
+            <h1>Suppression du mot clé</h1>
         </div>
         <div class="col-md-12">
             <!-- Form to create a new statut -->
             <form action="<?php echo ROOT_URL . '/api/keywords/delete.php' ?>" method="post">
                 <div class="form-group">
-                    <label for="libMotCle">Nom de la thematique</label>
+                    <label for="libMotCle">Nom du mot clé</label>
                     <input id="numMotCle" name="numMotCle" class="form-control" style="display: none" type="text" value="<?php echo($numMotCle); ?>" readonly="readonly" />
                     <input id="libMotCle" name="libMotCle" class="form-control" type="text" value="<?php echo($libMotCle); ?>" readonly="readonly" disabled />
                 </div>
