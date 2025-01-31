0bcc4a6293ad1ee4a478a4c7b50b45fd82e95695
Leon Carter <leoncarter4200@gmail.com>
2025-01-31 12:52:20 +0100

    Members API update.php

--- api/members/update.php
+++ api/members/update.php
@@ -1,2 +1,15 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
 
+$nomMemb = ctrlSaisies($_POST['nomMemb']);
+$prenomMemb = ctrlSaisies($_POST['prenomMemb']);
+$pseudoMemb = ctrlSaisies($_POST['pseudoMemb']);
+$passMemb = ctrlSaisies($_POST['passMemb']);
+$eMailMemb = ctrlSaisies($_POST['eMailMemb']);
+$numStat = ctrlSaisies($_POST['numStat']);
+
+sql_update('membre', 'nomMemb, prenomMemb, pseudoMemb, passMemb, eMailMemb, numStat', "'$nomMemb', '$prenomMemb', '$pseudoMemb', '$passMemb', '$eMailMemb', '$numStat'");
+
+
+header('Location: ../../views/backend/members/list.php');
