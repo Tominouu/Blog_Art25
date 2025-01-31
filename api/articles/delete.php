d1ce79595d7cdd468c9fec5494d4adf6dfcde6d6
Tom Leclercq <tomleclercq2006@gmail.com>
2025-01-31 16:13:10 +0100

    Update delete.php
    LECLERCQ, FLOUREZ

--- api/articles/delete.php
+++ api/articles/delete.php
@@ -1,2 +1,23 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
 
+if (isset($_POST['numArt'])) {
+    $numArt = ctrlSaisies($_POST['numArt']);
+    $article = sql_select('ARTICLE', 'urlPhotArt', "numArt = $numArt");
+
+    if (!empty($article) && isset($article[0]['urlPhotArt'])) {
+        $imagePath = $_SERVER['DOCUMENT_ROOT'] . '/src/uploads/' . $article[0]['urlPhotArt'];
+        if (file_exists($imagePath)) {
+            unlink($imagePath);
+        }
+    }
+
+    sql_delete('ARTICLE', "numArt = $numArt");
+    header('Location: ../../views/backend/articles/list.php?delete=success');
+    exit();
+} else {
+    header('Location: ../../views/backend/articles/list.php?delete=error');
+    exit();
+}
+?>
