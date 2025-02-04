5b5a43318d33317a13a9bd5717dd2f1148c78caf
Tom Leclercq <tomleclercq2006@gmail.com>
2025-02-04 12:35:45 +0100

    Update login.php

--- api/security/login.php
+++ api/security/login.php
@@ -1,2 +1,30 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
+session_start();
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $pseudo = ctrlSaisies($_POST['pseudo']);
+    $password = ctrlSaisies($_POST['password']);
+
+    // Vérifier si l'utilisateur existe avec ce pseudo
+    $user = sql_select("MEMBRE", "*", "pseudoMemb = '$pseudo'");
+
+    if ($user) {
+        if (password_verify($password, $user[0]['passMemb'])) {
+            $_SESSION['user_id'] = $user[0]['numMemb'];
+            $_SESSION['pseudoMemb'] = $user[0]['pseudoMemb'];
+
+            header("Location: " . ROOT_URL . "/index.php");
+            $_SESSION['pseudoMemb'] = $pseudo; // Stocke le pseudo en session
+            exit();
+        } else {
+            header("Location: " . ROOT_URL . "/views/security/login.php?error=Mot de passe incorrect");
+            exit();
+        }
+    } else {
+        header("Location: " . ROOT_URL . "/views/security/login.php?error=Pseudo ou mot de passe incorrect");
+        exit();
+    }
+}
+?>
