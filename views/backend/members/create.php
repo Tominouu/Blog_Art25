d626d5a4c23ca2c6299f25c0c8ca8f42442235a4
Lili-Jeanne <lili0jeanne0flourez@gmail.com>
2025-02-04 15:50:46 +0100

    Update create.php

--- views/backend/members/create.php
+++ views/backend/members/create.php
@@ -1,50 +1,24 @@
 <?php
-include '../../../header.php';
-?>
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
 
-<!-- Bootstrap form to create a new motcle -->
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Création nouveau Membre</h1>
-        </div>
-        <div class="col-md-12">
-            <!-- Form to create a new motcle -->
-            <form action="<?php echo ROOT_URL . '/api/members/create.php' ?>" method="post">
-                <div class="form-group">
-                    <label for="prenomMemb">Prénom</label>
-                    <input id="prenomMemb" name="prenomMemb" class="form-control" type="text" autofocus="autofocus" />
-                </div>
-                <div class="form-group">
-                    <label for="nomMemb">Nom</label>
-                    <input id="nomMemb" name="nomMemb" class="form-control" type="text" autofocus="autofocus" />
-                </div>
-                <div class="form-group">
-                    <label for="pseudoMemb">Pseudo</label>
-                    <input id="pseudoMemb" name="pseudoMemb" class="form-control" type="text" autofocus="autofocus" />
-                </div>
-                <div class="form-group">
-                    <label for="passMemb">Mot de Passe</label>
-                    <input id="passMemb" name="passMemb" class="form-control" type="text" autofocus="autofocus" />
-                </div>
-                <div class="form-group">
-                    <label for="eMailMemb">Email</label>
-                    <input id="eMailMemb" name="eMailMemb" class="form-control" type="text" autofocus="autofocus" />
-                </div>
-                <div class="form-group">
-                    <label for="numStat">Statut</label>
-                    <select id="numStat" name="numStat" class="form-control">
-                        <option value="1">Admin</option>
-                        <option value="2">Moderateur</option>
-                        <option value="3">Membre</option>
-                    </select>
-                </div>
-                <br />
-                <div class="form-group mt-2">
-                    <a href="list.php" class="btn btn-primary">List</a>
-                    <button type="submit" class="btn btn-success">Confirmer create ?</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
+$numMemb = ctrlSaisies($_POST['numMemb']);
+$nomMemb = ctrlSaisies($_POST['nomMemb']);
+$prenomMemb = ctrlSaisies($_POST['prenomMemb']);
+$pseudoMemb = ctrlSaisies($_POST['pseudoMemb']);
+$passMemb = ctrlSaisies($_POST['passMemb']);
+$eMailMemb = ctrlSaisies($_POST['eMailMemb']);
+
+
+
+//sql_delete('STATUT', "numStat = $numStat");
+sql_update('membre', "nomMemb = '$nomMemb'", "numMemb = $numMemb");
+sql_update('membre', "prenomMemb = '$prenomMemb'", "numMemb = $numMemb");
+sql_update('membre', "pseudoMemb = '$pseudoMemb'", "numMemb = $numMemb");
+sql_update('membre', "passMemb = '$passMemb'", "numMemb = $numMemb");
+sql_update('membre', "eMailMemb = '$eMailMemb'", "numMemb = $numMemb");
+// sql_update('membre', "numStat = '$numStat'", "numMemb = $numMemb");
+
+
+header('Location: ../../views/backend/members/list.php');
+?>
