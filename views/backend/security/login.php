8f6a8a37d161abf3dc400ccd5297f8a33fde11c2
Jordaneuh <jordan.tcha@mmibordeaux.com>
2025-02-04 12:07:38 +0100

    Update login.php (C'est juste un formulaire mais y'a pas de vérification)

--- views/backend/security/login.php
+++ views/backend/security/login.php
@@ -1,5 +1,23 @@
 <?php
 include '../../../header.php';
+include 'api/views/backend/secrity/styles-login.css';
+include 'api/views/backend/members/list.php'
 
-echo("Form login");
+ 
+// echo("Form login");
+?>
 
+
+<div class="box">
+    <div>
+        <label for="Pseudo-login">Pseudo</label>
+        <input type="text" name="Pseudo-login" id="Pseudo-login" />
+    </div>
+    <div>
+        <label for="pass-login">Mot de Passe</label>
+        <input type="password" name="pass-login" id="pass-login" />
+    </div>
+    <div class="form-group mt-2">
+                    <button type="submit" class="btn btn-primary m-1">Confirmer ?</button>
+                </div>
+</div>
