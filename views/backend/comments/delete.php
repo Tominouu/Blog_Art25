bacedf6ff20a474123db64d36b80a0e4986bdfa5
Jordaneuh <jordan.tcha@mmibordeaux.com>
2025-02-04 15:12:12 +0100

    Update delete.php rajouter les éléments que vous voulez

--- views/backend/comments/delete.php
+++ views/backend/comments/delete.php
@@ -1,3 +1,38 @@
 <?php
 include '../../../header.php';
 
+if(isset($_GET['numCom'])){
+    $numCom = $_GET['numCom'];
+    $dtCreaCom = sql_select("comment", "dtCreaCom", "numCom = $numCom")[0]['dtCreaCom'];
+    $libCom = sql_select("comment", "libCom", "numCom = $numCom")[0]['libCom'];
+    $dtModCom = sql_select("comment", "dtModCom", "numCom = $numCom")[0]['dtModCom'];
+    $dtDelLogCom = sql_select("comment", "dtDelLogCom", "numCom = $numCom")[0]['dtDelLogCom'];
+    $delLogiq = sql_select("comment", "delLogiq", "numCom = $numCom")[0]['delLogiq'];
+    $numArt = sql_select("comment", "numArt", "numCom = $numCom")[0]['numArt'];
+    $numMemb = sql_select("comment", "numMemb", "numCom = $numCom")[0]['numMemb'];
+}
+?>
+
+<!-- Bootstrap form to create a new statut -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Suppression du commentaire</h1>
+        </div>
+        <div class="col-md-12">
+            <!-- Form to create a new statut -->
+            <form action="<?php echo ROOT_URL . '/api/comments/delete.php' ?>" method="post">
+                <div class="form-group">
+                    <label for="numCom">Le commentaire</label>
+                    <input id="numCom" name="numCom" class="form-control" style="display: none" type="text" value="<?php echo($numCom); ?>" readonly="readonly" />
+                    <input id="libCom" name="libCom" class="form-control" type="text" value="<?php echo($libCom); ?>" readonly="readonly" disabled />
+                </div>
+                <br />
+                <div class="form-group mt-2">
+                    <a href="list.php" class="btn btn-primary">Delete</a>
+                    <button type="submit" class="btn btn-danger">Confirmer delete ?</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
