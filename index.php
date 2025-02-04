9734f919596a4fb524281d9e797b90bfd766a1ca
Tom Leclercq <tomleclercq2006@gmail.com>
2025-02-04 12:34:32 +0100

    Update index.php
    LECLERCQ

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php 
 require_once 'header.php';
+session_start(); 
 sql_connect();
 
-
+$pseudo = isset($_SESSION['pseudoMemb']) ? $_SESSION['pseudoMemb'] : null;
 ?>
 
+<div class="container">
+    <?php if ($pseudo): ?>
+        <h2>Bienvenue, <?php echo htmlspecialchars($pseudo); ?> !</h2>
+    <?php else: ?>
+        <h1>Bienvenue sur notre Blog</h1>
+        <p><a href="views/backend/security/login.php">Connectez-vous</a> pour accéder à votre espace.</p>
+    <?php endif; ?>
+</div>
 
-
-
-<?php require_once 'footer.php'; ?>
+<?php require_once 'footer.php'; ?> 
