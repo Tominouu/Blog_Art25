bd17398eb3a99076489fda89e6dce477f6634b18
Jordaneuh <jordan.tcha@mmibordeaux.com>
2025-02-04 14:46:19 +0100

    Update update.php modif admin commentaire

--- api/comments/update.php
+++ api/comments/update.php
@@ -1,2 +1,12 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
 
+$numCom = ctrlSaisies($_POST['numCom']);
+$libCom = ctrlSaisies($_POST['libCom']);
+
+//sql_delete('STATUT', "numStat = $numStat");
+sql_update('comment', "libCom = '$libCom'", "numCom = $numCom");
+
+header('Location: ../../views/backend/comments/list.php');
+?>
