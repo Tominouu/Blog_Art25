768eeafe3ad2a1ee980933d5a04c9cd48f38e7a9
Jordaneuh <jordan.tcha@mmibordeaux.com>
2025-02-04 09:36:38 +0100

    Update update.php (Code ok mais il reste le numStat)

--- api/members/update.php
+++ api/members/update.php
@@ -2,22 +2,23 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once '../../functions/ctrlSaisies.php';
 
+$numMemb = ctrlSaisies($_POST['numMemb']);
 $nomMemb = ctrlSaisies($_POST['nomMemb']);
 $prenomMemb = ctrlSaisies($_POST['prenomMemb']);
 $pseudoMemb = ctrlSaisies($_POST['pseudoMemb']);
 $passMemb = ctrlSaisies($_POST['passMemb']);
 $eMailMemb = ctrlSaisies($_POST['eMailMemb']);
-$numStat = isset($_POST['numStat']);
 
-$numMemb = ctrlSaisies($_POST['numMemb']);
 
-$sql = "UPDATE members SET 
-nomMemb = '$nomMemb', 
-prenomMemb = '$prenomMemb', 
-pseudoMemb = '$pseudoMemb', 
-passMemb = '$passMemb', 
-eMailMemb = '$eMailMemb', 
-numStat = '$numStat'
-WHERE numMemb = '$numMemb'";
+
+//sql_delete('STATUT', "numStat = $numStat");
+sql_update('membre', "nomMemb = '$nomMemb'", "numMemb = $numMemb");
+sql_update('membre', "prenomMemb = '$prenomMemb'", "numMemb = $numMemb");
+sql_update('membre', "pseudoMemb = '$pseudoMemb'", "numMemb = $numMemb");
+sql_update('membre', "passMemb = '$passMemb'", "numMemb = $numMemb");
+sql_update('membre', "eMailMemb = '$eMailMemb'", "numMemb = $numMemb");
+// sql_update('membre', "numStat = '$numStat'", "numMemb = $numMemb");
+
 
 header('Location: ../../views/backend/members/list.php');
+?>
