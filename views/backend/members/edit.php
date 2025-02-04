b181f6a336c19e49a4c1294d59ac7624fe4aa7e6
Jordaneuh <jordan.tcha@mmibordeaux.com>
2025-02-04 09:37:23 +0100

    Update edit.php (code ok mais il reste le numStat)

--- views/backend/members/edit.php
+++ views/backend/members/edit.php
@@ -8,43 +8,48 @@ if(isset($_GET['numMemb'])){
     $pseudoMemb = sql_select("membre", "pseudoMemb", "numMemb = $numMemb")[0]['pseudoMemb'];
     $passMemb = sql_select("membre", "passMemb", "numMemb = $numMemb")[0]['passMemb'];
     $eMailMemb = sql_select("membre", "eMailMemb", "numMemb = $numMemb")[0]['eMailMemb'];
+    $dtCreaMemb = sql_select("membre", "dtCreaMemb", "numMemb = $numMemb")[0]['dtCreaMemb'];
 }
 ?>
 
+<!-- Bootstrap form to create a new statut -->
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Modification Membre</h1>
+            <h1>Modification statut</h1>
         </div>
         <div class="col-md-12">
+            <!-- Form to create a new statut -->
             <form action="<?php echo ROOT_URL . '/api/members/update.php' ?>" method="post">
-                <fieldset>
-                    <legend>Informations du Membre</legend>
-                    <div class="form-group">
-                        <label for="prenomMemb">Prénom</label>
-                        <input id="prenomMemb" name="prenomMemb" class="form-control" type="text" value="<?php echo $prenomMemb; ?>"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="nomMemb">Nom</label>
-                        <input id="nomMemb" name="nomMemb" class="form-control" type="text" value="<?php echo $nomMemb; ?>"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="pseudoMemb">Pseudo</label>
-                        <input id="pseudoMemb" name="pseudoMemb" class="form-control" type="text" value="<?php echo $pseudoMemb; ?>"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="passMemb">Password</label>
-                        <input id="passMemb" name="passMemb" class="form-control" type="text" value="<?php echo $passMemb; ?>"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="eMailMemb">Mail</label>
-                        <input id="eMailMemb" name="eMailMemb" class="form-control" type="email" value="<?php echo $eMailMemb; ?>"/>
-                    </div>
-                </fieldset>
+                <div class="form-group">
+                    <label for="prenomMemb">Prénom</label>
+                    <input id="numMemb" name="numMemb" class="form-control" style="display: none" type="text" value="<?php echo($numMemb); ?>" readonly="readonly" />
+                    <input id="prenomMemb" name="prenomMemb" class="form-control" type="text" value="<?php echo($prenomMemb);  ?>"/>
+                </div>
+                <div class="form-group">
+                    <label for="nomMemb">Nom</label>
+                    <input id="numMemb" name="numMemb" class="form-control" style="display: none" type="text" value="<?php echo($numMemb); ?>" readonly="readonly" />
+                    <input id="nomMemb" name="nomMemb" class="form-control" type="text" value="<?php echo($nomMemb);  ?>"/>
+                </div>
+                <div class="form-group">
+                    <label for="pseudoMemb">Pseudo</label>
+                    <input id="numMemb" name="numMemb" class="form-control" style="display: none" type="text" value="<?php echo($numMemb); ?>" readonly="readonly" />
+                    <input id="pseudoMemb" name="pseudoMemb" class="form-control" type="text" value="<?php echo($pseudoMemb);  ?>"/>
+                </div>
+                <div class="form-group">
+                    <label for="pseudoMemb">Mot de Passe</label>
+                    <input id="numMemb" name="numMemb" class="form-control" style="display: none" type="text" value="<?php echo($numMemb); ?>" readonly="readonly" />
+                    <input id="passMemb" name="passMemb" class="form-control" type="text" value="<?php echo($passMemb);  ?>"/>
+                </div>
+                <div class="form-group">
+                    <label for="eMailMemb">Mail</label>
+                    <input id="numMemb" name="numMemb" class="form-control" style="display: none" type="text" value="<?php echo($numMemb); ?>" readonly="readonly" />
+                    <input id="eMailMemb" name="eMailMemb" class="form-control" type="text" value="<?php echo($eMailMemb);  ?>"/>
+                </div>
                 <br />
                 <div class="form-group mt-2">
-                    <a href="list.php" class="btn btn-primary">Edit</a>
-                    <button type="submit" class="btn btn-danger">Confirmer update ?</button>
+                    <a href="list.php" class="btn btn-primary">List</a>
+                    <button type="submit" class="btn btn-success">Confirmer la modification ?</button>
                 </div>
             </form>
         </div>
