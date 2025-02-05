6394e2f6b963694f51d2ae2f123edca70c025528
Jordaneuh <jordan.tcha@mmibordeaux.com>
2025-02-05 17:04:40 +0100

    Update update.php LA BONNE

--- api/comments/update.php
+++ api/comments/update.php
@@ -4,9 +4,16 @@ require_once '../../functions/ctrlSaisies.php';
 
 $numCom = ctrlSaisies($_POST['numCom']);
 $libCom = ctrlSaisies($_POST['libCom']);
+$delLogiq = ctrlSaisies($_POST['delLogiq']);
+$attModOK = ctrlSaisies($_POST['attModOK']);
+$notifComKOAff = ctrlSaisies($_POST['notifComKOAff']);
+
 
 //sql_delete('STATUT', "numStat = $numStat");
 sql_update('comment', "libCom = '$libCom'", "numCom = $numCom");
+sql_update('comment', "delLogiq = '$delLogiq'", "numCom = $numCom");
+sql_update('comment', "attModOK = '$attModOK'", "numCom = $numCom");
+sql_update('comment', "notifComKOAff = '$notifComKOAff'", "numCom = $numCom");
 
 header('Location: ../../views/backend/comments/list.php');
 ?>
