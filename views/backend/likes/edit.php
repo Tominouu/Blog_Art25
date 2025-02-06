eecf3820171112950a288a4a16ee6f9f7cf51999
Tom Leclercq <tomleclercq2006@gmail.com>
2025-02-06 22:11:37 +0100

    Likes admin (j'ai ajouté le début de la modération)

--- views/backend/likes/edit.php
+++ views/backend/likes/edit.php
@@ -1,3 +1,33 @@
 <?php
 include '../../../header.php';
 
+if(isset($_GET['numMemb'])){
+    $numMemb = $_GET['numMemb'];
+    $numArt = sql_select("likeart", "numArt", "numMemb = $numMemb")[0]['numArt'];
+}
+?>
+
+<!-- Bootstrap form to create a new likeart -->
+<link rel="stylesheet" href="/../../src/css/style.css">
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>modification likes</h1>
+        </div>
+        <div class="col-md-12">
+            <!-- Form to create a new likeart -->
+            <form action="<?php echo ROOT_URL . '/api/likes/create.php' ?>" method="post">
+                <div class="form-group">
+                    <label for="numArt">Nom du likeart</label>
+                    <input id="numMemb" name="numMemb" class="form-control" style="display: none" type="text" value="<?php echo ($numMemb); ?>" readonly="readonly" />
+                    <input id="numArt" name="numArt" class="form-control" type="text" value="<?php echo ($numArt); ?>"/>
+                </div>
+                <br />
+                <div class="form-group mt-2">
+                    <a href="list.php" class="btn btn-primary">Edit</a>
+                    <button type="submit" class="btn btn-danger">Confirmer changements ?</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
--- views/backend/likes/list.php
+++ views/backend/likes/list.php
@@ -1,3 +1,40 @@
 <?php
 include '../../../header.php'; // contains the header and call to config.php
 
+//charge tous les likes
+$likes = sql_select("likeart", "*");
+?>
+
+<!-- Bootstrap default layout to display all statuts in foreach -->
+<link rel="stylesheet" href="/../../src/css/style.css">
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Gestion des likes</h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Id Membre</th>
+                        <th>Nom de l'article</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($likes as $like){ ?>
+                        <tr>
+                            <td><?php echo($like['numMemb']); ?></td>
+                            <td><?php echo($like['numArt']); ?></td>
+                            <td>
+                                <a href="edit.php?numArt=<?php echo($like['numArt']); ?>&numMemb=<?php echo($like['numMemb']); ?>"class="btn btn-primary">Edit</a>
+                                <a href="delete.php?numArt=<?php echo($like['numArt']); ?>&numMemb=<?php echo($like['numMemb']); ?>" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+            <a href="create.php" class="btn btn-success">Create</a>
+        </div>
+    </div>
+</div>
+<?php
+include '../../../footer.php'; // contains the footer
